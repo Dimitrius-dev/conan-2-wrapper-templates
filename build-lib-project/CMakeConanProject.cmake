3f72bb986bdd2307f7b0999d1c23ff0e9087f6e9
Dmitrii Belov <dimabelovsergeevich@gmail.com>
2024-02-02 02:16:58 +0300

    added lib_flags option for lib, added explicit conan profile indication

--- build-lib-project/CMakeConanProject.cmake
+++ build-lib-project/CMakeConanProject.cmake
@@ -6,7 +6,10 @@ execute_process(
         COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/conanfile.py ${CMAKE_BINARY_DIR}/conanfile.py
         COMMAND ${conan} install ${CMAKE_BINARY_DIR}
         -s build_type=${CMAKE_BUILD_TYPE}
-        --output-folder=${CMAKE_BINARY_DIR}/conan_files --build=missing RESULT_VARIABLE OUTPUT
+        --output-folder=${CMAKE_BINARY_DIR}/conan_files --build=missing
+        -pr:b=${conan_profile}
+        -pr:h=${conan_profile}
+        RESULT_VARIABLE OUTPUT
 )
 
 include(${CMAKE_BINARY_DIR}/conan_files/conan_toolchain.cmake)
@@ -14,4 +17,6 @@ include(${CMAKE_BINARY_DIR}/conan_files/conan_toolchain.cmake)
 add_custom_target(create_lib
         COMMAND ${CMAKE_COMMAND} -E echo "--- create lib ---"
         COMMAND ${conan} create ${CMAKE_SOURCE_DIR}
+        -pr:b=${conan_profile}
+        -pr:h=${conan_profile}
 )
